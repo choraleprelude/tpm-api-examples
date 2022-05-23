4940e464397baf3c9ee5fa7f5d3affe3e3c97907
choraleprelude <choraleprelude@outlook.com>
2022-05-23 21:00:14 +0800

    cleaned up code comments

--- TCG.TSS.ESAPI/esapi_nvwrite_persistent.c
+++ TCG.TSS.ESAPI/esapi_nvwrite_persistent.c
@@ -11,9 +11,6 @@ Based on example from:
 #include <stdio.h>
 #include <string.h>
 
-// #include <openssl/bio.h>
-// #include <openssl/pem.h>
-
 #include <tss2/tss2_esys.h>
 #include <tss2/tss2_tctildr.h>
 #include <tss2/tss2_rc.h>
@@ -59,109 +56,6 @@ enum tool_rc {
     tool_rc_unsupported
 };
 
-// tool_rc tpm2_getcap(ESYS_CONTEXT *esys_context, TPM2_CAP capability,
-//         UINT32 property, UINT32 property_count, TPMI_YES_NO *more_data,
-//         TPMS_CAPABILITY_DATA **capability_data) {
-
-//     TSS2_RC rval = Esys_GetCapability(esys_context, ESYS_TR_NONE, ESYS_TR_NONE, ESYS_TR_NONE,
-//             capability, property, property_count, more_data, capability_data);
-//     if (rval != TSS2_RC_SUCCESS) {
-//         printf("Esys_GetCapability error: (0x%X) - %s\n", rval, Tss2_RC_Decode(rval));
-
-//         return 1;
-//     }
-
-//     return tool_rc_success;
-// }
-
-// #define NV_DEFAULT_BUFFER_SIZE 512
-
-// static inline tool_rc tpm2_util_nv_max_buffer_size(ESYS_CONTEXT *ectx,
-//         UINT32 *size) {
-
-//     /* Get the maximum read block size */
-//     TPMS_CAPABILITY_DATA *cap_data;
-//     TPMI_YES_NO more_data;
-//     tool_rc rc = tpm2_getcap(ectx, TPM2_CAP_TPM_PROPERTIES,
-//             TPM2_PT_NV_BUFFER_MAX, 1, &more_data, &cap_data);
-//     if (rc != tool_rc_success) {
-//         return rc;
-//     }
-
-//     if ( cap_data->data.tpmProperties.tpmProperty[0].property == TPM2_PT_NV_BUFFER_MAX ) {
-//         *size = cap_data->data.tpmProperties.tpmProperty[0].value;
-//     } else {
-//         /* TPM2_PT_NV_BUFFER_MAX is not part of the module spec <= 0.98*/
-//         *size = NV_DEFAULT_BUFFER_SIZE;
-//     }
-
-//     free(cap_data);
-
-//     return rc;
-// }
-
-// #define BAIL_ON_NULL(param, x) \
-//     do { \
-//         if (!x) { \
-//             printf(param" must be specified \n"); \
-//             return false; \
-//         } \
-//     } while(0)
-
-// static bool writex(FILE *f, UINT8 *data, size_t size) {
-
-//     size_t wrote = 0;
-//     size_t index = 0;
-//     do {
-//         wrote = fwrite(&data[index], 1, size, f);
-//         if (wrote != size) {
-//             if (errno != EINTR) {
-//                 return false;
-//             }
-//             /* continue on EINTR */
-//         }
-//         size -= wrote;
-//         index += wrote;
-//     } while (size > 0);
-
-//     return true;
-// }
-
-// bool files_write_bytes(FILE *out, uint8_t bytes[], size_t len) {
-
-//     BAIL_ON_NULL("FILE", out);
-//     BAIL_ON_NULL("bytes", bytes);
-//     return writex(out, bytes, len);
-// }
-
-// bool files_save_bytes_to_file(const char *path, UINT8 *buf, UINT16 size) {
-
-//     if (!buf) {
-//         return false;
-//     }
-
-//     if (!path) {
-//         return true;
-//     }
-
-//     FILE *fp = path ? fopen(path, "wb+") : stdout;
-//     if (!fp) {
-//         printf("Could not open file %s\n", path);
-//         return false;
-//     }
-
-//     bool result = files_write_bytes(fp, buf, size);
-//     if (!result) {
-//         printf("Could not write data to file %s\n", path);
-//     }
-
-//     if (fp != stdout) {
-//         fclose(fp);
-//     }
-
-//     return result;
-// }
-
 int main(int argc, char *argv[]) {
 
     TSS2_RC rv;
@@ -170,9 +64,6 @@ int main(int argc, char *argv[]) {
         printf("Usage: esapi_nvwrite_persistent hierarchy hierarchyauth NVindex NVauth input_path NVsize tcti (e.g.: esapi_nvwrite_persistent o ownerauth 10 nvauthpassword inputfile.txt 256 mssim)\n");
         return 1;
     }
-
-    // int rand_size;
-    // sscanf (argv[1],"%d",&rand_size);
 
     /* Prepare TCTI context */
     const char *tcti_name = argv[7];
@@ -226,17 +117,6 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-/*
-    // prepare persistent handle
-	TPMI_DH_PERSISTENT persist_handle;	
-    bool result = tpm2_util_string_to_uint32(argv[3], &persist_handle);
-    if (!result) {
-        fprintf(stderr, "Could not convert persistent handle to a number");
-        exit(1);
-    } else {
-		printf("persist_handle: %#x\n", persist_handle);
-	}
-*/
     // set NV auth
     TPM2B_AUTH h_NVAuthValue = {
         .size = 0,
@@ -290,114 +170,6 @@ int main(int argc, char *argv[]) {
         printf("Esys_NV_DefineSpace error: (0x%X) - %s\n", rv, Tss2_RC_Decode(rv));
 		return 1;
 	}
-
-/*     
-    // prepare NV handle
-    ESYS_TR NVHandle = ESYS_TR_NONE;    
-
-    rv = Esys_TR_FromTPMPublic(ectx, persist_handle,
-                              ESYS_TR_NONE, ESYS_TR_NONE, ESYS_TR_NONE,
-                              &NVHandle);    
-    if (rv != TSS2_RC_SUCCESS) {
-        printf("Esys_TR_FromTPMPublic error: (0x%X) - %s\n", rv, Tss2_RC_Decode(rv));
-		return 1;
-	}
-
-    // set NV object auth
-    TPM2B_AUTH nv_authValue = {
-        .size = 0,
-        .buffer = {0}
-    };    
-
-    if (strcmp(argv[2], "NULL") != 0) {
-        char *nv_auth = argv[2];
-
-        nv_authValue.size = strlen(nv_auth);
-        for (int i = 0; i < nv_authValue.size; i++) {
-            nv_authValue.buffer[i] = nv_auth[i];
-        }
-    }
-
-    rv = Esys_TR_SetAuth(ectx, NVHandle, &nv_authValue);
-    if (rv != TSS2_RC_SUCCESS) {
-		fprintf(stderr, "TR_SetAuth error: 0x%x\n", rv);
-		return 1;
-	} 
-*/
-
-/* 
-    // Read NV public data
-    TPM2B_NV_PUBLIC *nv_public = NULL;
-    // assume NV name = NULL;
-    rv = Esys_NV_ReadPublic(ectx, NVHandle,
-            ESYS_TR_NONE, ESYS_TR_NONE, ESYS_TR_NONE, &nv_public, NULL);
-    if (rv != TSS2_RC_SUCCESS) {
-        printf("Esys_NV_ReadPublic error: (0x%X) - %s\n", rv, Tss2_RC_Decode(rv));
-		return 1;
-	}
-
-    UINT16 data_size = nv_public->nvPublic.dataSize;
-    free(nv_public);
-
-    // if size is 0, assume the whole object 
-    UINT16 size;
-    if (size == 0) {
-        size = data_size;
-    }
-
-    printf ("datasize = %d \n", size);
-
-    UINT32 max_data_size = NV_DEFAULT_BUFFER_SIZE;
-
-    // read NV data
-    UINT8* data_buffer;    
-    UINT16 bytes_written;
-
-    data_buffer = malloc(data_size);
-    if (!data_buffer) {
-        printf("oom malloc \n");
-        return 1;
-    }
-
-    UINT16 offset = 0;
-    UINT16 data_offset = 0;
-
-    while (size > 0) {
-
-        UINT16 bytes_to_read = size > max_data_size ? max_data_size : size;
-
-        TPM2B_MAX_NV_BUFFER *nv_data;
-
-        rc = Esys_NV_Read(ectx, NVHandle, NVHandle, ESYS_TR_PASSWORD,
-            ESYS_TR_NONE, ESYS_TR_NONE, bytes_to_read, offset, &nv_data);
-        if (rc != tool_rc_success) {
-            printf("Esys_NV_Read error: (0x%X) - %s\n", rc, Tss2_RC_Decode(rc));
-            return 1;
-        }
-
-        size -= nv_data->size;
-        offset += nv_data->size;
-
-        memcpy(data_buffer + data_offset, nv_data->buffer, nv_data->size);
-        data_offset += nv_data->size;
-
-        free(nv_data);
-    }
-
-    bytes_written = data_offset;
-
-    // Write to output file
-    if (!files_save_bytes_to_file(argv[3], data_buffer,
-            bytes_written)) {
-        printf("Failed to save to file \n");
-        return 1;
-    }
-
-    if (data_buffer != NULL) {
-        free(data_buffer);
-        data_buffer = NULL;
-    }     
-*/
 
     exit(0);
 }
